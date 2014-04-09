d885c31b98813a7c531dc7b11f812b45920399f3
Aditya <aditya@netroy.in>
2014-04-09 15:12:42 +0200

    run websql spec only for browsers that support it

--- tests/Backends/websql.spec.js
+++ tests/Backends/websql.spec.js
@@ -2,15 +2,20 @@ describe('Database/Backends/WebSQL', function () {
 
   'use strict';
 
+  // this suite is only for browsers that support WebSQL
+  if (!('openDatabase' in global)) {
+    return;
+  }
+
+  var WBDatabase = require('WBDatabase');
+  var clone = require('wunderbits.core').lib.clone;
+
   var dbInstance;
 
   var exampleTask = {'storeName': 'tasks', 'type':'Task','assignee_id':null,'completed':false,'starred':true,'note':'Note sldsdlkfj. sdlfkjsldkfj sldkfj sdf. sdlfkj sdflkj.','completed_at':null,'recurrence_type':'week','recurrence_count':1,'recurring_parent_id':null,'due_date':'2013-04-27','parent_id':null,'position':-10,'completed_by_id':null,'created_at':'2013-04-27T10:20:36Z','created_by_id':'AADDAABAAFE','deleted_at':null,'list_id':'inbox','owner_id':'AADDAABAAFE','title':'An example task with all attributes.','updated_at':'2013-04-27T10:23:58Z','user_id':'AADDAABAAFE','online_id':'ACDDABPTPqg','id':'lw6d6f126f6cd509b2046c29fd8573bf','hasSubtasks':true,'from_paste':true,'local_change':true};
 
   var dbName = 'specs-' + ~~(Math.random() * 10e4);
   var dbVersion = 10;
-
-  var WBDatabase = require('WBDatabase');
-  var clone = require('wunderbits.core').lib.clone;
 
   beforeEach(function (done) {
 
