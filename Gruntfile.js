94c5eb6d0053323278c4621ff493f0d16bdc0c89
adamrenklint <adam@renklint.com>
2014-04-09 14:42:29 +0200

    update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,35 +4,16 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-complexity');
-  grunt.loadNpmTasks('grunt-esformatter');
-  grunt.loadNpmTasks('kaapi');
 
   function config (name) {
     return grunt.file.readJSON('grunt/configs/' + name + '.json');
   }
 
   grunt.initConfig({
-
-    // Linting
     'jshint': config('jshint'),
-    'complexity': config('complexity'),
-
-    // specs
-    'kaapi/node': config('kaapi'),
-
-    'esformatter': {
-      'options': grunt.file.readJSON('.esformatter'),
-      'src': [
-        'public/**/*.js',
-        'tests/**/*.spec.js'
-      ]
-    }
-
+    'complexity': config('complexity')
   });
 
   grunt.registerTask('lint', ['jshint', 'complexity']);
-
-  grunt.registerTask('tests', ['kaapi/node']);
-
   grunt.registerTask('default', ['lint']);
 };
