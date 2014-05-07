dff003d5d6e37c94de95493d8a55ad3642949e59
Raymond May Jr <raymondmay@gmail.com>
2014-05-07 15:07:09 +0200

    fix bug in local storage get

--- public/localStorage/WBBrowserLocalStorage.js
+++ public/localStorage/WBBrowserLocalStorage.js
@@ -18,7 +18,7 @@ var WBBrowserLocalStorage = WBClass.extend({
 
     var deferred = new WBDeferred();
     var value = localStorage.getItem(key);
-    return deferred.resolve().promise(value);
+    return deferred.resolve(value).promise();
   },
 
   'setItem': function (key, value) {
