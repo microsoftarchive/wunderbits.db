013790c6ad3afa44f8e5fbcd2e588f5cf772aba5
Aditya <aditya@netroy.in>
2014-05-26 18:15:40 +0200

    core@1.9

--- dist/wunderbits.db.js
+++ dist/wunderbits.db.js
@@ -665,17 +665,47 @@ function clone (obj, isDeep) {
 module.exports = clone;
 
 },{}],13:[function(_dereq_,module,exports){
+// http://stackoverflow.com/a/21963136/933653
 'use strict';
 
-function replacer (match) {
-  var rand = Math.random() * 16 | 0;
-  var chr = (match === 'x') ? rand : (rand & 0x3 | 0x8);
-  return chr.toString(16);
+var ff = 0xff;
+var lut = [];
+for (var i = 0; i < 256; i++) {
+  lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
+}
+
+var random = Math.random;
+function randHex() {
+  return (random() * 0xffffffff | 0);
+}
+
+function section0 () {
+  var d0 = randHex();
+  return lut[d0 & ff] + lut[d0 >> 8 & ff] +
+           lut[d0 >> 16 & ff] + lut[d0 >> 24 & ff];
+}
+
+function section1 () {
+  var d1 = randHex();
+  return lut[d1 & ff] + lut[d1 >> 8 & ff] + '-' +
+         lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & ff];
+}
+
+function section2 () {
+  var d2 = randHex();
+  return lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & ff] + '-' +
+       lut[d2 >> 16 & ff] + lut[d2 >> 24 & ff];
+}
+
+function section3 () {
+  var d3 = randHex();
+  return lut[d3 & ff] + lut[d3 >> 8 & ff] +
+       lut[d3 >> 16 & ff] + lut[d3 >> 24 & ff];
 }
 
 function createUID (prefix) {
-  var uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, replacer);
-  return String(!prefix ? '' : prefix) + uid;
+  var uid = [section0(), section1(), section2(), section3()].join('-');
+  return (!prefix ? '' : prefix).toString() + uid;
 }
 
 module.exports = createUID;
