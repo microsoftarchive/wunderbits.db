3e00867483fbc953cb513c90af2953eb33707849
Chad Fowler <chad@chadfowler.com>
2014-06-25 10:08:26 +0200

    non-working test. @octatone can you tell me if this is possible to do?

---
+++ tests/localStorage/chrome-local-storage.spec.js
@@ -0,0 +1,29 @@
+describe('localStorage/Chrome', function () {
+  this.timeout(10000);
+
+  'use strict';
+
+  // FIXME check for chrome local storage?
+  //if (!(chrome && chrome.storage && chrome.storage.local)) {
+  if (!chrome) {
+    return;
+  } 
+  var storage;
+
+  var WBChromeLocalStorage = require('localStorage/WBChromeLocalStorage');
+
+  describe('basic functionality', function () {
+    it ('should not throw exceptions', function (done) {
+      var itemGet = function(val) {
+        expect(val.to.equal("a value"));
+        storage.deleteItem("a key").done(done);
+      }
+      var itemSet = function() {
+        storage.getItem("a key").done(itemGet);        
+      }
+
+      storage = new WBChromeLocalStorage()
+      storage.setItem("a key", "a value").done(itemSet);
+    });
+  })
+})
