6ee6a54439d14fdd24bac7dd36f5c2e232718f8d
Chad Fowler <chad@chadfowler.com>
2014-06-28 09:30:28 +0100

    lint

--- tests/localStorage/chrome-local-storage.spec.js
+++ tests/localStorage/chrome-local-storage.spec.js
@@ -13,31 +13,31 @@ describe('localStorage/Chrome', function () {
       var checkClear = function(val) {
         expect(val).to.equal(undefined);
         done();
-      }
+      };
 
       var tryToRetrieveItem = function() {
-        storage.getItem("something").done(checkClear);
-      }
+        storage.getItem('something').done(checkClear);
+      };
 
       var itemHasBeenSet = function() {
         storage.clear().done(tryToRetrieveItem);
-      }
+      };
 
       storage = new WBChromeLocalStorage();
-      storage.setItem("something", "else").done(itemHasBeenSet);
+      storage.setItem('something', 'else').done(itemHasBeenSet);
     });
 
     it ('Can add and retrieve an item', function (done) {
       var itemGet = function(val) {
-        expect(val).to.eql("a value");
-        storage.removeItem("a key").done(done);
-      }
+        expect(val).to.eql('a value');
+        storage.removeItem('a key').done(done);
+      };
       var itemSet = function() {
-        storage.getItem("a key").done(itemGet);        
-      }
+        storage.getItem('a key').done(itemGet);        
+      };
 
       storage = new WBChromeLocalStorage();
-      storage.setItem("a key", "a value").done(itemSet);
+      storage.setItem('a key', 'a value').done(itemSet);
     });
-  })
-})
+  });
+});
